80502939c801c9b8f16c2a33af7429ce5f67d6d5
John Vu <johntrinhvu@yahoo.com>
2023-06-05 19:33:25 -0700

    Step 1 lab complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+finches = [
+    {
+        "name": "Ruby",
+        "breed": "Zebra Finch",
+        "description": "Ruby is a lively and social finch with beautiful markings.",
+        "age": 2
+    },
+    {
+        "name": "Sunny",
+        "breed": "Gouldian Finch",
+        "description": "Sunny is a colorful finch known for its vibrant plumage.",
+        "age": 1
+    },
+    {
+        "name": "Charlie",
+        "breed": "Bengalese Finch",
+        "description": "Charlie is a small and energetic finch that loves to sing.",
+        "age": 3
+    }
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
